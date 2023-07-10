65e1f0f7e9040fb371524c71ff0e5b1580ba2cc9
Lauren Hutchinson <lhutchinson@microsoft.com>
2023-07-10 14:47:38 -0700

    debugging action 2

--- src/action.js
+++ src/action.js
@@ -1,17 +1,24 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const { context } = require("@actions/github");
-const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
-const octokit = github.getOctokit(GITHUB_TOKEN);
-
-const { pull_request } = context.payload;
 
 async function run() {
-  await octokit.rest.issues.createComment({
-    ...context.repo,
-    issue_number: pull_request.number,
-    body: "Thank you for submitting a pull request! We will try to review this as soon as we can.",
-  });
+  console.log("Hello World");
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  if (pull_request) {
+    await octokit.rest.issues.createComment({
+      ...context.repo,
+      issue_number: pull_request.number,
+      body: "thank you for submitting a pull request. We will try to review this as soon as possible.",
+    });
+  } else {
+    console.log("This action was not triggered by a pull request.");
+  }
 }
 
 run();
